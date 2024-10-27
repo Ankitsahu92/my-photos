3712f13fcb4780c2fd79aba9c212b7a4f7935cf5
Ankit <urankitsahu92@gmail.com>
2024-10-27 15:16:09 +0530

    dd

--- API/controllers/readFileNamesController.js
+++ API/controllers/readFileNamesController.js
@@ -1,12 +1,35 @@
 const testFolder = "../src/assets/marriagePhotos/";
 const fs = require("fs");
 //readFileNamesRoutes
+const allFiles = [
+  "haldi ceremony selected",
+  "mehndi ceremony selected",
+  "mundap",
+  "Other",
+];
 const get = (req, res) => {
   try {
-    const data = [];
-    fs.readdirSync(testFolder).forEach((file) => {
+    const data = {
+      [allFiles[0]]: [],
+      [allFiles[1]]: [],
+      [allFiles[2]]: [],
+      [allFiles[3]]: [],
+    };
+    fs.readdirSync(`${testFolder}haldi ceremony selected/`).forEach((file) => {
       //console.log(file);
-      data.push(file);
+      data[allFiles[0]].push(file);
+    });
+    fs.readdirSync(`${testFolder}mehndi ceremony selected/`).forEach((file) => {
+      //console.log(file);
+      data[allFiles[1]].push(file);
+    });
+    fs.readdirSync(`${testFolder}mundap/`).forEach((file) => {
+      //console.log(file);
+      data[allFiles[2]].push(file);
+    });
+    fs.readdirSync(`${testFolder}mundap/`).forEach((file) => {
+      //console.log(file);
+      data[allFiles[3]].push(file);
     });
     res.send({ status: "OK", data });
   } catch (error) {
--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from "./logo.svg";
-import "./App.css";
+import photosJSON from "./assets/Photos.json";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
+//http://localhost:8080/api/readFileNamesRoutes
+//https://dev.to/franklin030601/lazy-loading-in-images-with-react-js-4enm
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. test
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const folderName = "haldi ceremony selected";
+  const Photos = photosJSON[folderName].slice(0, 5).map((photo, i) => {
+    return (
+      <LazyLoadImage
+        key={`${i}-${photo}`}
+        src={require(`./assets/marriagePhotos/${folderName}/${photo}`)}
+        alt={`Image Alt-${photo}`}
+        className="img-lazy"
+        width={700}
+        height={500}
+        effect="blur" // opacity | black-and-white
+      />
+    );
+    // {
+    //   src: require(`./assets/marriagePhotos/Other/${photo}`),
+    // };
+  });
+  return <div>{Photos}</div>;
 }
 
 export default App;
